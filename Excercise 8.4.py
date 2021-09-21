070467f96e6b21a1d77e807b4c1fdce9272d4e40
HerrMataBrujas <89623173+HerrMataBrujas@users.noreply.github.com>
2021-09-20 23:48:11 -0500

    Actualización de ejercicio
    Problema:  [24] separa las palabras de cada línea, pero solo toma el primer caracter de cada elemento de la línea,
     y lo agrega a la lista, en lugar de agregar la palabra completa

--- Excercise 8.4.py
+++ Excercise 8.4.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 17:29:25 2021
+
+@author: Emilio
+"""
+
 #8.4 Open the file romeo.txt and read it line by line.
 #For each line, split the line into a list of words using the split() method.
 #The program should build a list of words. For each word on each line check
@@ -6,12 +13,16 @@
 #alphabetical order.
 
 fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "romeo.txt"
 fh = open(fname)
-lst =[]
-
+lst = list()
+#separa las palabras de las lineas y las agrupa en 4 listas, 1xlinea
 for line in fh:
-    line.split(" ")
-    lst.append(line.rstrip())
+    print(line.split())
+    #speparar palabras de cada linea; pero solo toma el 1er caracter
+    for word in line:
+        if not word in lst:
+            lst.append(word)
+        continue
 print(lst)
-
-#Falta separar cada elemento de la lista en strings
